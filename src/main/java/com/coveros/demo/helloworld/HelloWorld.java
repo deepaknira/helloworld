6a210d400a3511176c710831c3c210d25569ebad
deepaknira <deepaknira3@gmail.com>
2022-04-09 12:26:06 +0530

    f5

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld {
     final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
     final LocalDateTime now = LocalDateTime.now();
 
-    System.out.println("Hello, World mars i am a Devops Engineer! The current time is " + dtf.format(now));
+    System.out.println("Hello, World jupiter i am a Devops Engineer! The current time is " + dtf.format(now));
   }
 
 }
